52276b48e7d4598c3093d294272327da51875679
Tezigudo <godjangg@gmail.com>
2022-10-13 15:15:55 +0700

    added some models and django

--- demo/models.py
+++ demo/models.py
@@ -5,12 +5,31 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-class Topic(models.Model):
+class Food(models.Model):
     """Topic model"""
     title = models.CharField(max_length=100)
-    description = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.IntegerField("price")
 
-    def __str__(self):
-        return self.title
+class Bills(models.Model):
+    pass
+
+class Payment(models.Model):
+    """Entry model"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
+    bill = models.ForeignKey(Bills, on_del)
+
+    class Status_choice(models.TextChoices):
+        PAID = 'PAID', 'PAID'
+        UNPAID = 'UNPAID'
+
+    image = models.ImageField(
+
+# field_name = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, **options)
+# ImageField.upload_to
+
+    )
+
+
+
